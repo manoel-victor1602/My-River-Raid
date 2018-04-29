6a06467bba903bca05f2a1ffa9b09f78052ff978
manoel-victor1602 <mvfds.snf@uea.edu.br>
2018-04-28 23:43:44 -0400

    Sprites que faltavam e Aviao animado

---
+++ My_River_Raid/Assets/Jogador/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{ 
+    public Animator animador;
+
+    // Use this for initialization
+    void Start()
+    {
+        animador = GetComponent<Animator>();
+        animador.SetBool("voando_esquerda", false);
+        animador.SetBool("voando_direita", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(animador.GetBool("voando_esquerda"));
+        while(Input.GetKey(KeyCode.LeftArrow)){
+            voandoEsquerda();
+        }
+        while(Input.GetKey(KeyCode.RightArrow)){
+            voandoDireita();
+        }
+        
+    }
+    void parado()
+    {
+        animador.SetBool("voando_esquerda", false);
+        animador.SetBool("voando_direita", false);
+    }
+    void voandoEsquerda()
+    {
+        animador.SetBool("voando_esquerda", true);
+        animador.SetBool("voando_direita", false);
+    }
+    void voandoDireita()
+    {
+        animador.SetBool("voando_direita", true);
+        animador.SetBool("voando_esquerda", false);
+    }
+}
